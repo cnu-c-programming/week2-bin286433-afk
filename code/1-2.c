66ec0962ad817bf14dfd3e1ff5c9ed2f3504df24
bin286433-afk <bin286433@gmail.com>
2026-03-13 09:26:03 +0900

    Update 1-2.c

--- code/1-2.c
+++ code/1-2.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 int main()
 {
+    printf("% 10s\n", "name");
+    printf("% 10s\n", "id");
+    printf("% 10s\n", "value");
+    
+    printf("% 10s\n", "kim");
+    printf("%0 10d\n", 12);
+    printf("% 10.2f\n", 4.12);
+
+    printf("% 10s\n", "lee");
+    printf("%0 10d\n", 1922);
+    printf("% 10.2\n", 3.21);
+
+    printf("% 10s\n", "park");
+    printf("%0 10d\n", 432);
+    printf("% 10.2f\n", 1);
+
+    
+    
 
 
     return 0;
