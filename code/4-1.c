2aa055f3fd910376cd2a3de0a6de4683922c3fe4
bin286433-afk <bin286433@gmail.com>
2026-03-13 10:18:58 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,6 +2,25 @@
 
 int main()
 {
+    int num1;
+    int num2;
+    char tool;
+
+    scanf("%d %d %c", &num1, &num2, &tool);
+
+    switch(tool){
+        case '+' :
+            printf(num1+num2);
+            break;
+        case '-' :
+            printf(num1-num2);
+            break;
+        case '*' :
+            printf(num1*num2);
+            break;
+        case '/':
+            printf(num1/num2);
+            break;
 
     return 0;
 }
