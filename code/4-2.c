db39b31697f7df24b51a90799e8843d4290eafc4
bin286433-afk <bin286433@gmail.com>
2026-03-13 10:28:01 +0900

    Update 4-2.c

--- code/4-2.c
+++ code/4-2.c
@@ -4,7 +4,7 @@ int main()
 {
     for(i=1; i<10; i++){
         for(r=1; r<10; r++){
-            printf("%d * %d = %d", r, i, (r*i));
+            printf("%d * %d = %d", i, j, (r*i));
         }
     }
 
