7b21918585a581facb89973678a7fa2cebc51df6
bin286433-afk <bin286433@gmail.com>
2026-03-13 13:53:14 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,6 +2,16 @@
 
 int main()
 {
+    int num;
+    scanf("%d", &num);
+    if(n<=1) {
+        printf("false");
+    }
+    for(int i=2; i*i<=num; i++){
+        if(num%i==0){
+            printf("false");
+        }
+        else printf("true");
 
     return 0;
 }
